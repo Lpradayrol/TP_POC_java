e1d3004bdfb0afe168efd572bf12439aeeb7cc62
Lpradayrol <43320445+Lpradayrol@users.noreply.github.com>
2020-10-14 19:45:19 +0200

    Add files via upload

--- SeptNains.java
+++ SeptNains.java
@@ -3,9 +3,11 @@
 import java.util.ArrayList;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.*;
 
 public class SeptNains {
-    static private SimpleDateFormat sdf = new SimpleDateFormat("hh'h 'mm'mn 'ss','SSS's'");    
+    static private SimpleDateFormat sdf = 
+        new SimpleDateFormat("hh'h 'mm'mn 'ss','SSS's'");    
 
     public static void main(String[] args) throws InterruptedException {
         Date début = new Date(System.currentTimeMillis());
@@ -13,7 +15,8 @@ public class SeptNains {
         
         final BlancheNeige bn = new BlancheNeige();
         final int nbNains = 7;
-        final String noms [] = {"Simplet", "Dormeur",  "Atchoum", "Joyeux", "Grincheux",
+        final String noms [] = {"Simplet", "Dormeur",  "Atchoum", "Joyeux", 
+                                "Grincheux",
                                 "Prof", "Timide"};
         final Nain nain [] = new Nain [nbNains];
         for(int i = 0; i < nbNains; i++) nain[i] = new Nain(noms[i],bn);
@@ -29,7 +32,8 @@ public class SeptNains {
         } catch(InterruptedException exception) { 
             exception.printStackTrace();
         } finally {
-            System.out.println("[" + sdf.format(new Date(System.currentTimeMillis())) + "] Interruption 7 nains.");
+            System.out.println("[" + sdf.format(new Date(System.currentTimeMillis()))
+                 + "] Interruption 7 nains.");
             for(int n = 0; n < nbNains; n++){
                 nain[n].interrupt();
             }
@@ -40,19 +44,43 @@ public class SeptNains {
             nain[n].join();
         }
 
-        System.out.println("[" + sdf.format(new Date(System.currentTimeMillis())) + "] Fin des nains.");
+        System.out.println("[" + sdf.format(new Date(System.currentTimeMillis())) 
+            + "] Fin des nains.");
     }
 }    
 
 class BlancheNeige {
-    private volatile boolean libre = true;        // Initialement, Blanche-Neige est libre.
+
+    /* file d'attente pour un partage équilibré pour l'accès à la ressource
+    * http://blog.paumard.org/cours/java-api/chap05-concurrent-queues.html
+    */
+    private BlockingQueue<Nain> file_acces = new ArrayBlockingQueue<Nain>(7);
+
+
+    private volatile boolean libre = true; // Initialement, Blanche-Neige est libre.
     public synchronized void requérir () {
-        System.out.println("\t" + Thread.currentThread().getName()
+
+        //le nain courant est ajouté à la file car il souhaite un accès à la ressource
+        Nain threadCourant = (Nain) Thread.currentThread();
+        file_acces.add(threadCourant);
+
+        System.out.println("\t" + threadCourant.getName()
                            + " veut la ressource.");
     }
 
     public synchronized void accéder () throws InterruptedException {
-        if ( ! libre ) wait();                    // Le nain s'endort sur l'objet bn
+
+        Nain threadCourant = (Nain) Thread.currentThread();
+
+        /* il faut maintenant vérifier que Blanche-Neige est libre 
+        * pour donner accès au nain s'il est premier dans la file de priorité
+         https://www.geeksforgeeks.org/arrayblockingqueue-peek-method-in-java/
+          (ref. peek())
+        */
+
+        while ( ! libre || !threadCourant.equals(file_acces.peek())) {
+            wait(); // Le nain s'endort sur l'objet bn
+        }                    
         libre = false;
         System.out.println("\t" + Thread.currentThread().getName()
                            + " accède à la ressource.");
@@ -62,6 +90,9 @@ class BlancheNeige {
         System.out.println("\t" + Thread.currentThread().getName()
                            + " relâche la ressource.");
         libre = true;
+
+        //le nain relâche la ressource, il faut donc le retirer de la file d'attente
+        file_acces.poll();
         notifyAll();
     }
 }
@@ -69,7 +100,8 @@ class BlancheNeige {
 class Nain extends Thread {
     private BlancheNeige bn;
 
-    static private SimpleDateFormat time = new SimpleDateFormat("hh'h 'mm'mn 'ss','SSS's'");    
+    static private SimpleDateFormat time = 
+        new SimpleDateFormat("hh'h 'mm'mn 'ss','SSS's'");    
 
     public Nain(String nom, BlancheNeige bn) {
         this.setName(nom);
@@ -78,61 +110,40 @@ class Nain extends Thread {
 
     public void run() {
         /*Si le nain non interrompu, il peut avoir accès à la ressource*/
-        while(!isInterrupted()) {
+        while(!isInterrupted()){
             try {
                 bn.requérir();
                 bn.accéder();
-                System.out.println("[" + time.format(new Date(System.currentTimeMillis())) + "]" + getName() + " a un accès (exclusif) à Blanche-Neige.");
+                System.out.println("[" + time.format(
+                        new Date(System.currentTimeMillis())) + "]" 
+                        + getName() 
+                        + " a un accès (exclusif) à Blanche-Neige.");
 
-                try {
-                    sleep(2000);
-                } catch (InterruptedException exception){
-                    this.interrupt();
-                } finally {
-                    System.out.println("[" + time.format(new Date(System.currentTimeMillis())) + "]" + getName() + " s'apprête à quitter Blanche-Neige.");
-                    bn.relâcher();
-                }
-            } catch(InterruptedException exception){
+                try { sleep(2000); 
+                } catch (InterruptedException exception1) {
+                    long temps_interruption = System.currentTimeMillis();
+                    long temps = System.currentTimeMillis() - temps_interruption;
+
+                        try { sleep(temps); 
+                        } catch (InterruptedException exception2) { 
+                            this.interrupt(); 
+                        } finally { this.interrupt(); } 
+
+                    } finally { 
+                    System.out.println("[" + time.format(
+                        new Date(System.currentTimeMillis())) + "] " 
+                        + getName() 
+                        + " s'apprête à quitter Blanche-Neige.");
+                    bn.relâcher(); }
+            } catch(InterruptedException exception3) {
                 this.interrupt();
             }
+
+            //interruption nains, message de fin
+            System.out.println("[" + time.format(
+                new Date(System.currentTimeMillis())) 
+                + "]" + getName() 
+                + " a terminé!");
         }
-
-        //interruption nain, message de fin
-        System.out.println("[" + time.format(new Date(System.currentTimeMillis())) + "]" + getName() + " a terminé!");
-    }
-        
-    //System.out.println(getName() + " a terminé!");
+    }   
 } 
-
-/*
-$ make
-$ java SeptNains
-[09h 34mn 01,834s] Début du programme.
-	Simplet veut la ressource.
-	Simplet accède à la ressource.
-	Timide veut la ressource.
-	Prof veut la ressource.
-	Grincheux veut la ressource.
-	Joyeux veut la ressource.
-	Atchoum veut la ressource.
-	Dormeur veut la ressource.
-Simplet a un accès (exclusif) à Blanche-Neige.
-Simplet s'apprête à quitter à Blanche-Neige.
-	Simplet relâche la ressource.
-	Simplet veut la ressource.
-	Simplet accède à la ressource.
-Simplet a un accès (exclusif) à Blanche-Neige.
-	Timide accède à la ressource.
-Timide a un accès (exclusif) à Blanche-Neige.
-	Dormeur accède à la ressource.
-Dormeur a un accès (exclusif) à Blanche-Neige.
-	Atchoum accède à la ressource.
-Atchoum a un accès (exclusif) à Blanche-Neige.
-	Joyeux accède à la ressource.
-Joyeux a un accès (exclusif) à Blanche-Neige.
-	Grincheux accède à la ressource.
-Grincheux a un accès (exclusif) à Blanche-Neige.
-	Prof accède à la ressource.
-Prof a un accès (exclusif) à Blanche-Neige.
-^C
-*/
